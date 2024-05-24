97d2d4299912e2b0abac8b5f65339a1d197967fb
satsuki329 <k021g1414@g.neec.ac.jp>
2024-05-24 13:25:38 +0900

    03_01完成

--- PG3_1_1/main.cpp
+++ PG3_1_1/main.cpp
@@ -1,7 +1,54 @@
 #include<stdio.h>
+#include<list>
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	char str[] = "ソ";
-	printf("%s", str);
+	list<string> lst{"Tokyo","kanda","Akihabara","Okachimachi","Ueno","Uguisudani","Nippori","Tabata","Komagome","Sugamo","Otuka","Ikebukuro","Mejiro","takadanobaba",
+		"Shin-Okubo","Shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatucho","Shimbashi","Yurakucho"};
+
+	list<string>::iterator itr;
+
+	printf("昭和3年の路線図(鉄道省時代)\n\n");
+
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+
+	for (itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = lst.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+	}
+
+	printf("\n昭和47年の路線図(国鉄時代)\n\n");
+
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+
+	for (itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		if (*itr == "Tamachi")
+		{
+			itr = lst.insert(itr, "Takanawa-Gateway");
+			++itr;
+		}
+	}
+
+	printf("\n令和5年の路線図(JR時代)\n\n");
+
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr)
+	{
+		std::cout << *itr << "\n";
+	}
+
+	return 0;
+
 }
