74aad47aba2b3100b0e08a1c6c5b562cf805f771
satsuki329 <k021g1414@g.neec.ac.jp>
2024-06-21 15:32:41 +0900

    保存

--- PG3_1_1/main.cpp
+++ PG3_1_1/main.cpp
@@ -1,7 +1,41 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
+class Animal
 {
-	char str[] = "ソ";
-	printf("%s", str);
+public:
+
+	virtual void Voice() {
+		printf("あにまる¥n");
+	};
+};
+
+class Cat : public Animal
+{
+public:
+	void Voice() override{
+		printf("にゃ～\n");
+	};
+};
+
+class Dog : public Animal
+{
+public:
+	void Voice() override {
+		printf("ワン！\n");
+	};
+};
+
+int main(void) {
+
+	Animal* animal[2] = { new Cat,new Dog };
+
+	animal[0]->Voice();
+	animal[1]->Voice();
+
+	delete animal[0];
+	delete animal[1];
+
+	return 0;
 }
+
+
